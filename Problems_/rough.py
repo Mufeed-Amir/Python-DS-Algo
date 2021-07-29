fb2bb77a34d4bbd12ebd25ca7d2586fcb68a8abc
Mufeed-Amir <mufeed.amir.17290@gmail.com>
2021-07-29 05:15:17 -0700

    working

--- Problems_/rough.py
+++ Problems_/rough.py
@@ -1,8 +1,15 @@
-import math as mt
-arr=[1,2,3,4,5,6,7,8,9]
+# import math as mt
+# arr=[1,2,3,4,5,6,7,8,9]
 
-nBucket=round(mt.sqrt(len(arr)))
+# nBucket=round(mt.sqrt(len(arr)))
 
-print(nBucket)
+# print(nBucket)
 
 
+# m=[2,3,4]
+# dm=dict(m)
+# print(dm)
+
+a={1:1,2:2,3:3}
+del a[2]
+print(a)
